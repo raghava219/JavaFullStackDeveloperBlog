553ff6e186057ea117ac71846e1de0acc56c51c1
Raghava <raghava219@gmail.com>
2025-07-20 20:42:24 +0530

    Updated login.component.ts

--- src/app/features/auth/login.component.ts
+++ src/app/features/auth/login.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
-import { User } from '../../core/models/user.model';
+import { User } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-login',
