290339274c72d1669fd9b023cc3ebe511839ddba
Raghava <raghava219@gmail.com>
2025-08-03 10:01:43 +0530

    Updated list-article.component.ts

---
+++ src/app/features/list/list-article.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { marked } from 'marked';
+import { Article } from '../../core/models/article.model';
+import { ArticleService } from '../../core/services/article.service';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css'
+})
+export class ArticleComponent implements OnInit {
+  article?: Article;
+  renderedContent = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
+
+  async ngOnInit() {
+    this.route.params.subscribe(async params => {
+      const slug = params['slug'];
+      this.articleService.getArticleBySlug(slug).subscribe(async article => {
+        if (article) {
+          this.article = article;
+          this.renderedContent = await marked.parse(article.content);
+        }
+      });
+    });
+  }
+}
