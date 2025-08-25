6415da2fee8840e3055a9cbe0cf36bd7364841ff
Raghava <raghava219@gmail.com>
2025-08-25 06:11:14 +0530

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'YOUR_SUPABASE_URL_HERE',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY_HERE'
+  supabaseUrl: 'https://sfkxqkimfcoajbvbbfoj.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNma3hxa2ltZmNvYWpidmJiZm9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMzI2NjgsImV4cCI6MjA3MTYwODY2OH0.sqlH-ZkR5VipKXUZ6TF1rljb8xXzzic8AMtyaTPbXxM'
 };
